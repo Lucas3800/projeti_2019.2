55e77b027c48f0c9c5a2d1ce9d0a6ab80cbc8583
Lucas Almeida de Abreu <lucas3800@live.com>
2019-12-12 20:35:38 -0300

    Revert "Revert "interface atualizada""
    This reverts commit 84193e6c452e56d16bc0eb21d321a3cb68fe1e2e.

---
+++ site/js/automacao.js
@@ -0,0 +1,10 @@
+function mudarValor() {
+    if(document.getElementById('button').value == 'ativo') {
+
+    document.getElementById('button').value == 'inativo'
+    }
+    else {
+        document.getElementById('button').value == 'ativo'
+    }
+}
+
